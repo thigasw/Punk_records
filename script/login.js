acc93cf693095d6c2b636c69c18de5a973541a73
Thiago Wilkerson <thiagowilkerson20@gmail.com>
2026-02-16 22:07:56 -0300

    Adiciona-validacao-de-login

---
+++ script/login.js
@@ -0,0 +1,8 @@
+function logar() {
+  let user = document.getElementById("user").value;
+  let password = document.getElementById("password").value;
+
+  if (user == "admin" && password == "1234") {
+    alert("sucesso");
+  }
+}
